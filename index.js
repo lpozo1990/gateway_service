4f5edb3aebc09f8c9b0fbe9354f1a9d7d1949aeb
lpozo1990 <lpozo1990@gmail.com>
2023-07-22 07:44:01 -0400

    Add cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); 
 
 const app = express();
 const PORT = 3000; 
 
+
+app.use(cors()); 
 app.use(bodyParser.json());
 
 // In-memory database to store gateways and devices

abb69386305b887cacebf208cfe51de423b05e4b
lpozo1990 <lpozo1990@gmail.com>
2023-07-22 06:32:47 -0400

    Initial

---
+++ index.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000; 
+
+app.use(bodyParser.json());
+
+// In-memory database to store gateways and devices
+const gatewayDatabase = {
+  gateways: [],
+};
+
+function isValidIP(ip) {
+  return true; // Placeholder validation for simplicity
+}
+
+function isValidPeripheralDevice(device) {
+  // You can add more validation logic here if needed
+  return typeof device.vendor === 'string' && device.vendor.trim().length > 0;
+}
+
+function isGatewayValid(gateway) {
+  if (!isValidIP(gateway.ipv4)) {
+    return false;
+  }
+
+  if (gateway.devices && gateway.devices.length > 10) {
+    return false;
+  }
+
+  if (gateway.devices) {
+    return gateway.devices.every(isValidPeripheralDevice);
+  }
+
+  return true;
+}
+
+// Routes
+app.get('/gateways', (req, res) => {
+  res.json(gatewayDatabase.gateways);
+});
+
+app.post('/gateways', (req, res) => {
+  const gateway = req.body;
+  if (!isGatewayValid(gateway)) {
+    return res.status(400).json({ error: 'Invalid gateway data' });
+  }
+
+  gatewayDatabase.gateways.push(gateway);
+  res.status(201).json(gateway);
+});
+
+app.get('/gateways/:serialNumber', (req, res) => {
+  const { serialNumber } = req.params;
+  const gateway = gatewayDatabase.gateways.find((g) => g.serialNumber === serialNumber);
+  if (!gateway) {
+    return res.status(404).json({ error: 'Gateway not found' });
+  }
+
+  res.json(gateway);
+});
+
+app.put('/gateways/:serialNumber/add-device', (req, res) => {
+  const { serialNumber } = req.params;
+  const device = req.body;
+
+  if (!isValidPeripheralDevice(device)) {
+    return res.status(400).json({ error: 'Invalid device data' });
+  }
+
+  const gateway = gatewayDatabase.gateways.find((g) => g.serialNumber === serialNumber);
+  if (!gateway) {
+    return res.status(404).json({ error: 'Gateway not found' });
+  }
+
+  if (!gateway.devices) {
+    gateway.devices = [];
+  }
+
+  if (gateway.devices.length >= 10) {
+    return res.status(400).json({ error: 'Gateway has reached the maximum number of devices' });
+  }
+
+  gateway.devices.push(device);
+  res.status(201).json(gateway);
+});
+
+app.delete('/gateways/:serialNumber/remove-device/:uid', (req, res) => {
+  const { serialNumber, uid } = req.params;
+  const gateway = gatewayDatabase.gateways.find((g) => g.serialNumber === serialNumber);
+  if (!gateway) {
+    return res.status(404).json({ error: 'Gateway not found' });
+  }
+
+  if (!gateway.devices) {
+    return res.status(404).json({ error: 'No devices associated with the gateway' });
+  }
+
+  const deviceIndex = gateway.devices.findIndex((device) => device.uid === parseInt(uid, 10));
+  if (deviceIndex === -1) {
+    return res.status(404).json({ error: 'Device not found' });
+  }
+
+  gateway.devices.splice(deviceIndex, 1);
+  res.status(200).json(gateway);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ readme.md
@@ -0,0 +1,71 @@
+# Gateway Service API
+
+This is a REST service for storing information about gateways and their associated devices.
+
+## Requirements
+
+- Node.js (https://nodejs.org) installed on your system.
+
+## Installation
+
+1. Clone this repository to your local machine:
+
+git clone https://github.com/lpozo1990/gateway_service.git
+
+css
+Copy code
+
+2. Navigate to the project directory:
+
+cd gateway-service
+
+arduino
+Copy code
+
+3. Install dependencies using npm:
+
+npm install
+
+csharp
+Copy code
+
+## Usage
+
+1. Start the server using Nodemon:
+
+npm start
+
+markdown
+Copy code
+
+The server will run on http://localhost:3000.
+
+2. Interact with the API using tools like Postman (https://www.postman.com/downloads/) or your preferred HTTP client.
+
+## Endpoints
+
+- **GET /gateways**: Get information about all stored gateways and their devices.
+- **POST /gateways**: Store a new gateway in the database.
+- **GET /gateways/:serialNumber**: Get details for a specific gateway.
+- **PUT /gateways/:serialNumber/add-device**: Add a new device to a gateway.
+- **DELETE /gateways/:serialNumber/remove-device/:uid**: Remove a device from a gateway.
+
+## Models
+
+### Gateway
+
+- `serialNumber` (string) - The unique serial number of the gateway.
+- `name` (string) - Human-readable name of the gateway.
+- `ipv4` (string) - The IPv4 address of the gateway.
+- `devices` (array) - An array of associated peripheral devices.
+
+### Peripheral Device
+
+- `uid` (number) - The unique identifier of the peripheral device.
+- `vendor` (string) - The vendor of the peripheral device.
+- `dateCreated` (string) - The date the peripheral device was created.
+- `status` (string) - The status of the peripheral device (online/offline).
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
